ca6729834127469457282de0bdf57d23f4af47ee
mufasa1976 <mufasa1976@coolstuff.software>
2017-02-11 16:02:06 +0100

    StringUtils of commons3 instead of commons

--- src/main/java/software/coolstuff/springframework/owncloud/service/impl/local/OwncloudLocalResourceServiceImpl.java
+++ src/main/java/software/coolstuff/springframework/owncloud/service/impl/local/OwncloudLocalResourceServiceImpl.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
